8d92f637697df373664a1c9629de5a35a6bdbf88
TheZoq2 <frans.skarman@gmail.com>
2018-11-14 18:47:25 +0100

    Add parameter to copy_template to create agendas from the future

--- copy_template.sh
+++ copy_template.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-cat template.md \
-    | sed "s/{DATE}/$(LANG=sv_SE.UTF-8 date '+%-d %B %Y')/g" \
-    > "styrelsemote_$(date "+%y%m%d").md"
+ADDED_DAYS=0
+if [[ "$1" != "" ]]; then
+    ADDED_DAYS=$1
+fi
+
+CURRENT_DATE="$(date +%Y-%m-%d)"
+
+TARGET_DATE=$(date "+%Y-%m-%d" -d "$CURRENT_DATE + $ADDED_DAYS day")
+
+filename="styrelsemote_$(date "+%y%m%d" -d "$TARGET_DATE").md"
+
+
+sed "s/{DATE}/$(LANG=sv_SE.UTF-8 date -d "$TARGET_DATE" '+%-d %B %Y')/g" \
+    > "$filename" \
+    < template.md
+
+echo "$filename"
 
